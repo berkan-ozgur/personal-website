d6847895199de6e437af1c5d5cff5b6adf15e029
Berkan Özgür <berkanozgur@hotmail.com>
2022-12-17 01:51:59 +0300

    Navbar added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,38 @@
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import './App.scss';
 function App() {
   return (
     <div className="container">
       <div className="row">
-        <div className="App">
-          <title>Berkan Özgür</title>
-          <div className="about">
-            <ul>
-              <li>
-                Berkan ÖZGÜR
-              </li>
-              <li>
-                Front-end Developer
-              </li>
-            </ul>
-            <a href="https://www.linkedin.com/in/berkan-ozgur/">
-              <div className="social">
-                <i className="fa-brands fa-linkedin-in"></i>
-              </div>
-            </a>
-            <a href="https://www.instagram.com/berkan.ozgr/">
-              <div className="social">
-                <i className="fa-brands fa-instagram"></i>
-              </div>
-            </a>
-            <a href="https://github.com/berkan-ozgur/">
-              <div className="social">
-                <i className="fa-brands fa-github"></i>
-              </div>
-            </a>
-          </div>
+        <title>Berkan Özgür</title>
+        <div className="navbar">
+          <a href="">Ana Sayfa</a>
+          <a href="#about">Hakkımda</a>
+          <a href="/">Portfolyo</a>
+        </div>
+        <div className="about">
+          <ul>
+            <li>
+              Berkan ÖZGÜR
+            </li>
+            <li>
+              Front-end Developer
+            </li>
+          </ul>
+          <a href="https://www.linkedin.com/in/berkan-ozgur/">
+            <div className="social">
+              <i className="fa-brands fa-linkedin-in"></i>
+            </div>
+          </a>
+          <a href="https://www.instagram.com/berkan.ozgr/">
+            <div className="social">
+              <i className="fa-brands fa-instagram"></i>
+            </div>
+          </a>
+          <a href="https://github.com/berkan-ozgur/">
+            <div className="social">
+              <i className="fa-brands fa-github"></i>
+            </div>
+          </a>
         </div>
       </div>
     </div>
